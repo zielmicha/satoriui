08c4af7df87307dcff88474ec6844a7e72d858a9
butataatawa <jan.derbisz@gmail.com>
2015-05-14 17:06:37 +0200

    Napisy

--- gui/src/net/atomshare/satori/gui/SubmitScreen.java
+++ gui/src/net/atomshare/satori/gui/SubmitScreen.java
@@ -18,25 +18,6 @@ import javafx.scene.text.Font;
 import javafx.scene.text.FontWeight;
 import javafx.scene.text.Text;
 import javafx.stage.Stage;
-
-import javafx.application.Application;
-import javafx.event.ActionEvent;
-import javafx.event.EventHandler;
-import javafx.geometry.Insets;
-import javafx.geometry.Pos;
-import javafx.scene.Scene;
-import javafx.scene.control.Button;
-import javafx.scene.control.Label;
-import javafx.scene.control.PasswordField;
-import javafx.scene.control.TextField;
-import javafx.scene.layout.GridPane;
-import javafx.scene.layout.HBox;
-import javafx.scene.layout.StackPane;
-import javafx.scene.paint.Color;
-import javafx.scene.text.Font;
-import javafx.scene.text.FontWeight;
-import javafx.scene.text.Text;
-import javafx.stage.Stage;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.scene.control.*;
@@ -56,26 +37,31 @@ public class SubmitScreen extends Application{
         grid.setPadding(new Insets(25, 25, 25, 25));
 
         final Text actiontarget = new Text();
-        grid.add(actiontarget, 1, 6);
+        grid.add(actiontarget, 1, 4);
+
+        final Text actiontarget2 = new Text();
+        actiontarget2.setFill(Color.RED);
+        actiontarget2.setText("Nie wybrano pliku");
+        grid.add(actiontarget2, 1, 2);
 
         Button btn = new Button("Wyślij");
         HBox hbBtn = new HBox(10);
         hbBtn.setAlignment(Pos.BOTTOM_RIGHT);
         hbBtn.getChildren().add(btn);
-        grid.add(hbBtn, 1, 4);
+        grid.add(hbBtn, 2, 4);
 
         Button btn2 = new Button("Wybierz");
         HBox hbBtn2 = new HBox(10);
         hbBtn2.setAlignment(Pos.BOTTOM_RIGHT);
         hbBtn2.getChildren().add(btn2);
-        grid.add(hbBtn2, 1, 2);
+        grid.add(hbBtn2, 2, 2);
 
         ListView<String> list = new ListView<String>();
         ObservableList<String> items =FXCollections.observableArrayList (
                 "A", "B", "C", "D");
         list.setItems(items);
-        list.setPrefSize(200, 70);
-        grid.add(list, 1, 1);
+        list.setPrefSize(210, 30);
+        grid.add(list, 1, 1, 2, 1);
 
         btn.setOnAction(new EventHandler<ActionEvent>() {
             @Override
@@ -85,7 +71,15 @@ public class SubmitScreen extends Application{
             }
         });
 
-        Scene scene = new Scene(grid, 400, 250);
+        btn2.setOnAction(new EventHandler<ActionEvent>() {
+            @Override
+            public void handle(ActionEvent e) {
+                actiontarget2.setFill(Color.BLUE);
+                actiontarget2.setText("Nazwa_Pliku");
+            }
+        });
+
+        Scene scene = new Scene(grid, 400, 225);
         primaryStage.setScene(scene);
 
         Text scenetitle = new Text("Wyślij rozwiązanie");
