4627d696a0da309eb00b0e327a6bb8433a28a62b
butataatawa <jan.derbisz@gmail.com>
2015-05-14 15:24:41 +0200

    Lista

--- gui/src/net/atomshare/satori/gui/SubmitScreen.java
+++ gui/src/net/atomshare/satori/gui/SubmitScreen.java
@@ -19,11 +19,33 @@ import javafx.scene.text.FontWeight;
 import javafx.scene.text.Text;
 import javafx.stage.Stage;
 
+import javafx.application.Application;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.geometry.Insets;
+import javafx.geometry.Pos;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.GridPane;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.StackPane;
+import javafx.scene.paint.Color;
+import javafx.scene.text.Font;
+import javafx.scene.text.FontWeight;
+import javafx.scene.text.Text;
+import javafx.stage.Stage;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.scene.control.*;
+
 public class SubmitScreen extends Application{
-	public static void main(String[] args) {
+    public static void main(String[] args) {
         launch(args);
-	}
-	@Override
+    }
+    @Override
     public void start(Stage primaryStage) {
         primaryStage.setTitle("Wysyłanie rozwiązań");
 
@@ -41,6 +63,19 @@ public class SubmitScreen extends Application{
         hbBtn.setAlignment(Pos.BOTTOM_RIGHT);
         hbBtn.getChildren().add(btn);
         grid.add(hbBtn, 1, 4);
+
+        Button btn2 = new Button("Wybierz");
+        HBox hbBtn2 = new HBox(10);
+        hbBtn2.setAlignment(Pos.BOTTOM_RIGHT);
+        hbBtn2.getChildren().add(btn2);
+        grid.add(hbBtn2, 1, 2);
+
+        ListView<String> list = new ListView<String>();
+        ObservableList<String> items =FXCollections.observableArrayList (
+                "A", "B", "C", "D");
+        list.setItems(items);
+        list.setPrefSize(200, 70);
+        grid.add(list, 1, 1);
 
         btn.setOnAction(new EventHandler<ActionEvent>() {
             @Override
@@ -60,14 +95,11 @@ public class SubmitScreen extends Application{
         Label userName = new Label("Wybierz zadanie:");
         grid.add(userName, 0, 1);
 
-        TextField userTextField = new TextField();
-        grid.add(userTextField, 1, 1);
 
         Label pw = new Label("Wybierz plik:");
         grid.add(pw, 0, 2);
 
-        PasswordField pwBox = new PasswordField();
-        grid.add(pwBox, 1, 2);
+
 
         primaryStage.show();
     }
