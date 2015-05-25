96e49fedbce0b77e63c13062709632f4cb34a2dd
butataatawa <jan.derbisz@gmail.com>
2015-05-25 11:14:06 +0200

    Create Problems.java

---
+++ gui/src/net/atomshare/satori/gui/Problems.java
@@ -0,0 +1,78 @@
+package net.atomshare.satori.gui;
+import javafx.application.Application;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.geometry.Insets;
+import javafx.geometry.Pos;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.GridPane;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.StackPane;
+import javafx.scene.paint.Color;
+import javafx.scene.text.Font;
+import javafx.scene.text.FontWeight;
+import javafx.scene.text.Text;
+import javafx.stage.Stage;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.scene.control.*;
+
+public class Problems extends Application{
+    public static void main(String[] args) {
+        launch(args);
+    }
+    @Override
+    public void start(Stage primaryStage) {
+        primaryStage.setTitle("Lista problemów");
+
+        GridPane grid = new GridPane();
+        grid.setAlignment(Pos.CENTER);
+        grid.setHgap(10);
+        grid.setVgap(10);
+        grid.setPadding(new Insets(25, 25, 25, 25));
+
+
+        Button btn = new Button("Wyświetl treść");
+        HBox hbBtn = new HBox(10);
+        hbBtn.setAlignment(Pos.BOTTOM_RIGHT);
+        hbBtn.getChildren().add(btn);
+        grid.add(hbBtn, 2, 3);
+
+        ListView<String> list = new ListView<String>();
+        ObservableList<String> items =FXCollections.observableArrayList (
+                "A  Nazwa1", "B  Nazwa2", "C  Nazwa3", "D  Nazwa 4");
+        list.setItems(items);
+        list.setPrefSize(210, 30);
+        grid.add(list, 1, 1, 2, 1);
+
+
+
+        btn.setOnAction(new EventHandler<ActionEvent>() {
+
+            @Override
+            public void handle(ActionEvent e) {
+
+            }
+        });
+
+        Scene scene = new Scene(grid, 400, 225);
+        primaryStage.setScene(scene);
+
+        Text scenetitle = new Text("Problemy");
+        scenetitle.setFont(Font.font("Tahoma", FontWeight.NORMAL, 20));
+        grid.add(scenetitle, 0, 0, 2, 1);
+
+        Label userName = new Label("Zadanie:");
+        grid.add(userName, 0, 1);
+
+
+
+
+        primaryStage.show();
+    }
+}
+
